df322c87682ab6afa66163a2be4bfec07edca543
Jhyea026 <joaohenrique26.1@gmail.com>
2023-11-25 20:49:49 -0300

    tela principal com app e bottomNavigation

---
+++ lib/Widgets/card.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, prefer_const_literals_to_create_immutables, must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class HomeCards extends StatefulWidget {
+  String titulo;
+  List<Widget> conteudo;
+  HomeCards({super.key, required this.titulo, required this.conteudo});
+
+  @override
+  State<HomeCards> createState() => _CardsState();
+}
+
+class _CardsState extends State<HomeCards> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20, right: 26, left: 26),
+      height: 159,
+      width: 360,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: Color.fromARGB(255, 191, 191, 191))),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: Column(
+          children: [
+            Text(
+              widget.titulo,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+            Column(
+              children: widget.conteudo,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Windows/home.dart
+++ lib/Windows/home.dart
@@ -1,15 +1,42 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-class Home extends StatefulWidget {
+import 'package:flutter/material.dart';
+import 'package:handfinance/Colors/cor.dart';
+import 'package:handfinance/Widgets/card.dart';
+
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        backgroundColor: Cor.Primary50,
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(bottom: 20),
+            child: Center(
+              child: Column(
+                children: [
+                  HomeCards(
+                    titulo: 'Resumo da sua carteira',
+                    conteudo: [],
+                  ),
+                  HomeCards(
+                    titulo: 'Cartões de credito',
+                    conteudo: [],
+                  ),
+                  HomeCards(
+                    titulo: 'Planejamento',
+                    conteudo: [],
+                  ),
+                  HomeCards(
+                    titulo: 'Economia mensal',
+                    conteudo: [],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
